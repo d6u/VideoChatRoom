f91c12e24f61fd25f72ae141b8cdd383aeca094f
Daiwei Lu <daiwei@fb.com>
2021-09-18 16:21:21 -0700

    add ec2 stun and turn server

--- raw_with_firebase/config.js
+++ raw_with_firebase/config.js
@@ -1,7 +1,10 @@
 const SERVERS = {
   iceServers: [
     {
-      urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302']
+      // urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302']
+      urls: ['stun:13.57.211.159:3478', 'turn:13.57.211.159:3478'],
+      username: "username2",
+      credential: "password2",
     }
   ],
   iceCandidatePoolSize: 10,
