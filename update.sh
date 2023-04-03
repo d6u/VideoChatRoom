eeb31edac5e5a41ec495738492f4787a4af4e1c0
Daiwei Lu <daiweilu123@gmail.com>
2023-04-03 10:01:56 -0700

    Rename

--- update.sh
+++ update.sh
@@ -3,18 +3,17 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-printf "\n>>> Cleaning up staging area for CloudFormation...\n"
-rm -rf build
-mkdir build
-
-# Enter /shared
+# Enter /shared/shared-models
 pushd shared/shared-models
+printf "\n>>> Update dependencies for shared-models...\n"
 rm -rfv dist node_modules | sed 's/\([^/]*\)\/.*$/\1/' | sort | uniq
 npm i
 tsc
 popd
 
+# Enter /shared/shared-utils
 pushd shared/shared-utils
+printf "\n>>> Update dependencies for shared-utils...\n"
 rm -rfv dist node_modules | sed 's/\([^/]*\)\/.*$/\1/' | sort | uniq
 npm i
 tsc
@@ -23,9 +22,22 @@ popd
 # Enter /server
 pushd server
 
-printf "\n>>> Cleaning up staging area for server...\n"
+printf "\n>>> Update dependencies for server...\n"
 rm -rfv dist node_modules | sed 's/\([^/]*\)\/.*$/\1/' | sort | uniq
 
 printf "\n>>> Building...\n"
 npm i
 tsc
+
+popd
+
+# Enter /frontend
+pushd frontend
+
+printf "\n>>> Update dependencies for front-end...\n"
+rm -rfv node_modules/shared-models | sed 's/\([^/]*\)\/.*$/\1/' | sort | uniq
+
+printf "\n>>> Building...\n"
+npm i
+
+popd
